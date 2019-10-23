9478204c60cbbf6d8569c78613d8214ecd02eef4
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-10-23 19:39:35 +0200

    Refactor: structure with HTML, JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,39 @@ const cardsData = [
   {
     cardHeadline: 'Supervisor',
     cardBody: 'Monitors activity to identify project roadblocks',
-    cardIconSrc: './images/icon-supervisor.svg'
+    cardIconSrc: './images/icon-supervisor.svg',
+    borderColor: 'cyan-border'
   },
   {
     cardHeadline: 'Team Builder',
     cardBody: 'Scans our talent network to create the optimal team for your project',
-    cardIconSrc: './images/icon-team-builder.svg'
+    cardIconSrc: './images/icon-team-builder.svg',
+    borderColor: 'red-border'
   },
   {
     cardHeadline: 'Karma',
     cardBody: 'Regularly evaluates our talent to ensure quality',
-    cardIconSrc: './images/icon-karma.svg'
+    cardIconSrc: './images/icon-karma.svg',
+    borderColor: 'orange-border'
   },
   {
     cardHeadline: 'Calculator',
     cardBody: 'Uses data from past projects to provide better delivery estimates',
-    cardIconSrc: './images/icon-calculator.svg'
+    cardIconSrc: './images/icon-calculator.svg',
+    borderColor: 'blue-border'
   }
 ];
 
-const cardContainer = document.getElementById('card-container');
+const cardContainerGrid = document.getElementById('card-container-grid');
 
 function addCardSection(cardsData) {
   cardsData.forEach(cardData => {
     // create card
     const card = document.createElement('section');
-    card.setAttribute('class', 'card');
-    cardContainer.appendChild(card);
+    card.setAttribute('class', `card ${cardData.borderColor} section-padding`);
+
+    // card.style.borderTop = `4px solid ${cardData.borderColor}`; // add inline style not recommended
+    cardContainerGrid.appendChild(card);
 
     // create, add card data
     const cardHeadline = document.createElement('h4');
@@ -37,6 +43,7 @@ function addCardSection(cardsData) {
     card.appendChild(cardHeadline);
 
     const cardBody = document.createElement('p');
+    cardBody.setAttribute('class', 'card-body');
     cardBody.innerHTML = cardData.cardBody;
     card.appendChild(cardBody);
 
